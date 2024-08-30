6e961399b77f3442f86c8c74ec7cd0d90ae1afd4
DarkKnight <94906343+l0v3m0n3y@users.noreply.github.com>
2024-08-30 22:36:17 +0300

    Update shikimori.js

--- shikimori.js
+++ shikimori.js
@@ -1,46 +1,46 @@
 class shikimori{
     constructor(){
         this.api = "https://shikimori.one/api"
-        this.headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"}
+        this.headerss={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"}
     }
     async anime_list(page,limit,order,kind,censored){
-        let req=await fetch(`${this.api}/animes?&page=${page}&limit=${limit}&order=${order}&kind=${kind}&censored=${censored}`,{method:"GET",headers: this.header});
+        let req=await fetch(`${this.api}/animes?&page=${page}&limit=${limit}&order=${order}&kind=${kind}&censored=${censored}`,{method:"GET",headerss: this.headers});
         return await req.json();
     }
     async calendar_list(censored){
-        let req=await fetch(`${this.api}/calendar?censored=${censored}`,{method:"GET",headers: this.header});
+        let req=await fetch(`${this.api}/calendar?censored=${censored}`,{method:"GET",headerss: this.headers});
         return await req.json();
     }
     async search_manga(page,limit,order,censored,search){
-        let req=await fetch(`${this.api}/mangas?&page=${page}&limit=${limit}&order=${order}&censored=${censored}&search=${search}`,{method:"GET",headers: this.header});
+        let req=await fetch(`${this.api}/mangas?&page=${page}&limit=${limit}&order=${order}&censored=${censored}&search=${search}`,{method:"GET",headerss: this.headers});
         return await req.json();
     }
     async search_anime(page,limit,order,censored,search){
-        let req=await fetch(`${this.api}/animes?&page=${page}&limit=${limit}&order=${order}&censored=${censored}&search=${search}`,{method:"GET",headers: this.header});
+        let req=await fetch(`${this.api}/animes?&page=${page}&limit=${limit}&order=${order}&censored=${censored}&search=${search}`,{method:"GET",headerss: this.headers});
         return await req.json();
     }
     async search_ranobe(page,limit,order,censored,search){
-        let req=await fetch(`${this.api}/ranobe?&page=${page}&limit=${limit}&order=${order}&censored=${censored}&search=${search}`,{method:"GET",headers: this.header});
+        let req=await fetch(`${this.api}/ranobe?&page=${page}&limit=${limit}&order=${order}&censored=${censored}&search=${search}`,{method:"GET",headerss: this.headers});
         return await req.json();
     }
     async manga_list(page,limit,order,kind,censored){
-        let req=await fetch(`${this.api}/mangas?&page=${page}&limit=${limit}&order=${order}&kind=${kind}&censored=${censored}`,{method:"GET",headers: this.header});
+        let req=await fetch(`${this.api}/mangas?&page=${page}&limit=${limit}&order=${order}&kind=${kind}&censored=${censored}`,{method:"GET",headerss: this.headers});
         return await req.json();
     }
     async ranobe_list(page,limit,order,kind,censored){
-        let req=await fetch(`${this.api}/ranobe?&page=${page}&limit=${limit}&order=${order}&kind=${kind}&censored=${censored}`,{method:"GET",headers: this.header});
+        let req=await fetch(`${this.api}/ranobe?&page=${page}&limit=${limit}&order=${order}&kind=${kind}&censored=${censored}`,{method:"GET",headerss: this.headers});
         return await req.json();
     }
     async manga_info(title){
-        let req=await fetch(`${this.api}/animes/${title}`,{method:"GET",headers: this.header});
+        let req=await fetch(`${this.api}/animes/${title}`,{method:"GET",headerss: this.headers});
         return await req.json();
     }
     async ranobe_info(title){
-        let req=await fetch(`${this.api}/ranobe/${title}`,{method:"GET",headers: this.header});
+        let req=await fetch(`${this.api}/ranobe/${title}`,{method:"GET",headerss: this.headers});
         return await req.json();
     }
     async anime_info(title){
-        let req=await fetch(`${this.api}/animes/${title}`,{method:"GET",headers: this.header});
+        let req=await fetch(`${this.api}/animes/${title}`,{method:"GET",headerss: this.headers});
         return await req.json();
     }
 }
